270791a7b0493f1153a5e8af39a0ab11b2344e88
SinonHecateII <sinonhecateii297@gmail.com>
2021-10-29 18:30:23 +0900

    2021-10-29  앱 내 메뉴 구성을 위한 코드 분할, 하단 메뉴 바 작업 진행

---
+++ StudyClassApp/StudyClass/src/pages/SearchScreen.js
@@ -0,0 +1,30 @@
+/*메인화면 Scene
+  앱 접속시 가장 기본으로되는 화면
+  */ 
+
+import React, {Component} from 'react';
+import{
+    StyleSheet,
+    Text,
+    View,
+    StatusBar
+}from 'react-native';
+
+export default function SearchScreen(navigation){
+    return(
+        <View style = {{
+            flex : 1,
+            alignItems : 'center',
+            justifyContent : 'center'
+        }}>
+            <Text
+                onPress = {() => navigation.navigate('Home')}
+                style = {{
+                    fontSize : 26, 
+                    fontWeight : 'bold'
+                    }}>
+                Search Screen
+            </Text>
+        </View>
+    );
+}
